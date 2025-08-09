3ad9b6edda05a99c7cf9817af3a7f89c97deff3d
ClearVibeSip <clearvibesip@gmail.com>
2025-08-09 09:58:32 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,19 +1,19 @@
-let slideIndex = 0;
-const slides = document.getElementsByClassName("slide");
-
-function showSlides() {
-  for (let slide of slides) {
-    slide.classList.remove("active");
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) slideIndex = 1;
-  slides[slideIndex - 1].classList.add("active");
-  setTimeout(showSlides, 4000);
-}
-
-window.onload = showSlides;
-
 document.addEventListener("DOMContentLoaded", () => {
+  let slideIndex = 0;
+  const slides = document.getElementsByClassName("slide");
+
+  function showSlides() {
+    for (let slide of slides) {
+      slide.classList.remove("active");
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) slideIndex = 1;
+    slides[slideIndex - 1].classList.add("active");
+    setTimeout(showSlides, 4000);
+  }
+
+  showSlides();
+
   const exploreBtn = document.getElementById("explore-btn");
   const dropdown = document.getElementById("explore-dropdown");
 
